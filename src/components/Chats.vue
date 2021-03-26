c021e2a482b0493699900ea95b6b72a1b6fa43d2
Yuu Chin <shontzuforever@gmail.com>
2021-03-27 00:36:56 +0800

    chat styling #1

--- src/components/Chats.vue
+++ src/components/Chats.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="wrapper">
     <div class="message" v-for="message in messages" v-bind:key="message._id">
-      <p class="text">{{ message.text }}</p>
-      <p class="time">{{ message.created_at }}</p>
-      <p class="user">{{ message.username }}</p>
+      <!-- how to use font-awwesome in Vue?? -->
+      <!-- import '@fortawesome/fontawesome-free/css/all.css' -->
+      <div style="width: 10vw" class="fa fa-user-circle fa-3x"></div>
+      <div style="width: 90vw">
+        <div class="chatLog">
+          <b>{{ message.username }}</b>
+          <i>{{ message.created_at }}</i>
+        </div>
+        <div class="chatBubble">
+          <p>{{ message.text }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +43,19 @@ export default {
 </script>
 <style>
 .message {
-  margin-left: 10px;
+  display: flex;
+  margin-bottom: 20px;
+}
+.chatLog {
+  display: flex;
+  justify-content: space-between;
+}
+.chatBubble {
+  padding:10px;
+  border:1px solid black;
+  border-radius: 5px  20px 20px;
+}
+p {
+  margin: 0;
 }
 </style>
